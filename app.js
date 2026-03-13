2742b9f96a97166bf51615cb746fbb978d6f0025
DEV-BYTE-SOURSE <developerjunior200@gmail.com>
2026-03-13 05:43:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const TARGET = 800;
+const TARGET =800;
 
 // DOM
 const submitBtn = document.getElementById("submitBtn");
